4e2d5113fe8fa06cbd6856582eeff2d828d261ee
elainafan <869812580@qq.com>
2025-05-22 14:07:36 +0800

    改变了编译器

---
+++ notebook_for_PKU_2025_practice-of-programming-in-cpp.pro
@@ -0,0 +1,13 @@
+QT += widgets webenginewidgets
+
+CONFIG += c++17
+
+SOURCES += \
+    main.cpp \
+    widget.cpp
+
+HEADERS += \
+    widget.h
+
+RESOURCES +=
+
---
+++ widget.h
@@ -0,0 +1,26 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+class QTextEdit;
+class QWebEngineView;
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+
+private slots:
+    void updatePreview();
+    void saveToFile();
+
+private:
+    QTextEdit *textEdit;
+    QWebEngineView *preview;
+    bool previewLoaded = false;
+};
+
+#endif // WIDGET_H
